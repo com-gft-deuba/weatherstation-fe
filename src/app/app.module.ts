5b6852ecd5575ec343af31a089fd1365e0f8233b
Emilio Tenorio <emilio.tenorio@gft.com>
2021-11-17 18:35:15 +0100

    modules imported and initial toolbar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { DeviceComponent } from './device/device.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +19,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
